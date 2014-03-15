b3f032ada8dbe2971a7abe17637b5e1e75577c5e
sirbotta <sbottecchia@gobalto.com>
2014-03-15 11:58:35 -0700

    first working commit

---
+++ compiler.rb
@@ -0,0 +1,68 @@
+require 'csv'
+require 'yaml'
+groups = YAML.load_file('groups.yml')
+
+### labels per classificare dalla riga d'imput nel csv
+row_lbl= {}
+groups.each do | key , group |
+  group.each do | label, row|
+    row_lbl[row]= label
+  end
+end 
+
+### labels associate ad un particolare gruppo
+groups_names= []
+group_lbl= Hash.new []
+groups.each do | key , group |
+  groups_names.push(key)
+  group.each do | label, row|
+    group_lbl[key]+= [].push(label) 
+  end
+end
+
+### trucco per crare hash di hash
+raw = Hash.new do |hash,key| 
+    hash[key] = {}
+end
+
+### funzione che data un hash di lbl: value restituisce la media di un certo gruppo passato come array di lables
+def lblmean(data,lblgroup)
+   sum=0.00
+   lblgroup.each do |lbl|
+     sum+= data[lbl].to_f
+   end
+   return sum / lblgroup.length
+end
+
+### leggo i dati, rigiro i subject in righe e associo correttamente alla riga la label (i subject partono da 1, non zero)
+CSV.foreach('input/Sat_CFA.xls') do |row|
+  row.each_with_index do | data , subject|
+    if(row_lbl.has_key?($INPUT_LINE_NUMBER))
+      raw[subject+1].merge!(row_lbl[$INPUT_LINE_NUMBER] => data)
+    end
+  end
+end
+
+
+
+### preparo l'output facendo le medie per gruppi di di ogni soggetto
+output = Hash.new []
+raw.each do |subject, value|
+   #printf "soggetto #{subject} |"
+   group_lbl.each do |group, labels|
+     #printf "#{group}-> #{lblmean(value,labels)} |"
+     output[subject] += [].push(lblmean(value,labels))
+   end
+   #printf "\n"
+end
+
+### scrivo il file csv nella cartella output
+CSV.open('output/medie_aggregate.csv', 'w') do |csv| 
+  csv << groups_names # Add new headers 
+  output.each do |subject , row| 
+    csv.puts row
+  end
+end
+
+
+
